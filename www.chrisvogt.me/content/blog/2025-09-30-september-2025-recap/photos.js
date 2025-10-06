d4369601a26e5fbc8170066bb429ae845b41daf7
Chris Vogt <mail@chrisvogt.me>
2025-10-05 22:21:47 -0700

    content(blog): add new recap for Sept 2025

---
+++ www.chrisvogt.me/content/blog/2025-09-30-september-2025-recap/photos.js
@@ -0,0 +1,227 @@
+// Placeholder photo arrays for September 2025 recap
+// Using picsum.photos for temporary placeholders - replace with actual photos when ready
+
+export const sonomaRoadTrip = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759727070/chrisvogt-me/galleries/now-sept-2025/IMG_5913-2.jpg',
+    title: 'Checking into R3 Hotel in Guerneville',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1759725697/chrisvogt-me/galleries/now-sept-2025/IMG_5786.jpg',
+    title: 'The pool at R3 Hotel in Guerneville during Labor Day weekend',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1759725697/chrisvogt-me/galleries/now-sept-2025/IMG_5797.jpg',
+    title: 'A pool party at R3 Hotel in Guerneville, Labor Day weekend, 2025',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759725696/chrisvogt-me/galleries/now-sept-2025/IMG_5800.jpg',
+    title: "Gay Camp, comedy that ran at NYC's The Duplex for five years, as well as Fire Island, Ptown, and on tour",
+    width: 4,
+    height: 3
+  }
+]
+
+export const sonomaWineries = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759725698/chrisvogt-me/galleries/now-sept-2025/IMG_5833.jpg',
+    title: 'Wine grapes up close, at Rodney Strong winery',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759725697/chrisvogt-me/galleries/now-sept-2025/IMG_5810.jpg',
+    title: 'Listening to music and drinking wine at Bacchus Landing in Healdsburg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759725697/chrisvogt-me/galleries/now-sept-2025/IMG_5825.jpg',
+    title: 'Wine tasting at Rodney Strong winery in Healdsburg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759725698/chrisvogt-me/galleries/now-sept-2025/IMG_5829.jpg',
+    title: 'Chris and Nick posing with an oversized, prop wine bottle at Rodney Strong winery in Healdsburg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759725699/chrisvogt-me/galleries/now-sept-2025/IMG_5852.jpg',
+    title: 'A row of grapevines at La Crema winery in Healdsburg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1759725700/chrisvogt-me/galleries/now-sept-2025/IMG_5858.jpg',
+    title: 'Nick drinking a glass of wine at La Crema winery in Healdsburg',
+    width: 3,
+    height: 4
+  }
+]
+
+export const sonomaDriveHome = [
+  {
+    src: 'https://picsum.photos/1200/900?random=30',
+    title: 'Highway 1 coastal drive',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=31',
+    title: 'Coastal stop for photos',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://picsum.photos/1200/900?random=32',
+    title: 'Souvenir shopping',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=33',
+    title: 'Final coastal views',
+    width: 3,
+    height: 4
+  }
+]
+
+export const vinylCollection = [
+  {
+    src: 'https://picsum.photos/1200/900?random=6',
+    title: 'Vinyl collection display in living room',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=7',
+    title: 'Record player setup',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://picsum.photos/1200/900?random=8',
+    title: 'New vinyl additions',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=9',
+    title: 'Music corner decoration',
+    width: 3,
+    height: 4
+  }
+]
+
+export const indoorGardening = [
+  {
+    src: 'https://picsum.photos/1200/900?random=10',
+    title: 'Countertop Aerogarden setup',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=11',
+    title: 'Floor Aerogarden with cucumbers and tomatoes',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://picsum.photos/1200/900?random=12',
+    title: 'Growing cucumber vine reaching for light',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=13',
+    title: 'Jalapeño plants growing',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://picsum.photos/1200/900?random=14',
+    title: 'Cherry tomatoes on the vine',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=15',
+    title: 'Herb garden in countertop Aerogarden',
+    width: 3,
+    height: 4
+  }
+]
+
+export const sanDiegoZoo = [
+  {
+    src: 'https://picsum.photos/1200/900?random=16',
+    title: 'Bear at San Diego Zoo',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=17',
+    title: 'Snake exhibit',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://picsum.photos/1200/900?random=18',
+    title: 'Elephant family',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=19',
+    title: 'Playful lemurs',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://picsum.photos/1200/900?random=20',
+    title: 'Zoo entrance and architecture',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=21',
+    title: 'Additional animal photos',
+    width: 3,
+    height: 4
+  }
+]
+
+export const friendsHighlights = [
+  {
+    src: 'https://picsum.photos/1200/900?random=22',
+    title: 'Dinner with friends',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=23',
+    title: 'Social gathering highlights',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://picsum.photos/1200/900?random=24',
+    title: 'Fun moments with friends',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://picsum.photos/900/1200?random=25',
+    title: 'September social highlights',
+    width: 3,
+    height: 4
+  }
+]
