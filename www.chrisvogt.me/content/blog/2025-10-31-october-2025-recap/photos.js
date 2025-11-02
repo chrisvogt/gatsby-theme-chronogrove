0bae35a473519298ef22de0621cb49288014f93f
Chris Vogt <mail@chrisvogt.me>
2025-11-02 01:49:51 -0700

    [squashme] add Castro Street Fair photos

---
+++ www.chrisvogt.me/content/blog/2025-10-31-october-2025-recap/photos.js
@@ -0,0 +1,44 @@
+export const castroStreetFair = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1762072223/chrisvogt-me/galleries/now-oct-2025/20251004-IMG_6431.jpg',
+    title: 'Nick, Salim and Mike, at The Mix, celebrating the new Halloween decorations',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1762072223/chrisvogt-me/galleries/now-oct-2025/20251004-IMG_6428.jpg',
+    title: "Nick and Mike at The Mix, and Mike's new rat friend",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1762072223/chrisvogt-me/galleries/now-oct-2025/20251004-IMG_6425.jpg',
+    title: "Ratter McRatface, caught at Cliff's Varieties in the Castro",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1762072223/chrisvogt-me/galleries/now-oct-2025/20251004-IMG_6437.jpg',
+    title: 'A new Día de Muertos mural off 24th Street in the SF Mission district',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1762072224/chrisvogt-me/galleries/now-oct-2025/20251005-IMG_6448.jpg',
+    title: 'Looking North on Castro Street for the Castro Street Fair, taken from 19th St',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1762072225/chrisvogt-me/galleries/now-oct-2025/20251005-IMG_6456.jpg',
+    title: 'Facing Twin Peaks and the rainbow flag over Harvey Milk Plaza from Market St during the Castro Street Fair',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1762072226/chrisvogt-me/galleries/now-oct-2025/20251005-IMG_6460.jpg',
+    title: 'Cheer SF, a non-profit, LGBTIA+ non-profit, at the Castro Street Fair',
+    width: 4,
+    height: 3
+  }
+]
