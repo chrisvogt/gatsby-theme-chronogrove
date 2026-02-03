5c703e36c786407bc5a59d341785d64a345a498d
Chris Vogt <mail@chrisvogt.me>
2026-02-02 19:19:17 -0800

    SQUASHME: work on replacing photos

--- www.chrisvogt.me/content/blog/2026-01-31-january-2026-recap/photos.js
+++ www.chrisvogt.me/content/blog/2026-01-31-january-2026-recap/photos.js
@@ -36,37 +36,37 @@ export const chinatown = [
 
 export const friendsInTheCity = [
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085939/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2426.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085939/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2426.jpg',
     title: 'Out with friends - Photo 1',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2456.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2456.jpg',
     title: 'Out with friends - Photo 2',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085917/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2458.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085917/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2458.jpg',
     title: 'Out with friends - Photo 3',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085914/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2472.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085914/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2472.jpg',
     title: 'Out with friends - Photo 4',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085937/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2542.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085937/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2542.jpg',
     title: 'Out with friends - Photo 5',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085943/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2545.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085943/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2545.jpg',
     title: 'Out with friends - Photo 6',
     width: 3,
     height: 4
@@ -75,43 +75,43 @@ export const friendsInTheCity = [
 
 export const outAndAbout = [
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085938/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2387.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085938/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2387.jpg',
     title: 'Out and about - Photo 1',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085941/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2419.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085941/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2419.jpg',
     title: 'Out and about - Photo 2',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085936/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2435.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085936/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2435.jpg',
     title: 'Out and about - Photo 3',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085916/chrisvogt-me/galleries/now-jan-2026/20260109-IMG_2499.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085916/chrisvogt-me/galleries/now-jan-2026/20260109-IMG_2499.jpg',
     title: 'Out and about - Photo 4',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085925/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2513.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085925/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2513.jpg',
     title: 'Out and about - Photo 5',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2518.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2518.jpg',
     title: 'Out and about - Photo 6',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085915/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2528.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085915/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2528.jpg',
     title: 'Out and about - Photo 7',
     width: 3,
     height: 4
@@ -121,90 +121,251 @@ export const outAndAbout = [
 export const palmSprings = [
   // Traveling to Palm Springs
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085912/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2587.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085912/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2587.jpg',
     title: 'Traveling to Palm Springs - Photo 1',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085912/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2593.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085912/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2593.jpg',
     title: 'Traveling to Palm Springs - Photo 2',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085911/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2595.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085911/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2595.jpg',
     title: 'Traveling to Palm Springs - Photo 3',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085936/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2599.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085936/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2599.jpg',
     title: 'Traveling to Palm Springs - Photo 4',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085941/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2615.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085941/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2615.jpg',
     title: 'Traveling to Palm Springs - Photo 5',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085914/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2623.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085914/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2623.jpg',
     title: 'Traveling to Palm Springs - Photo 6',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085913/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2632.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085913/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2632.jpg',
     title: 'Traveling to Palm Springs - Photo 7',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085922/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2641.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085922/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2641.jpg',
     title: 'Traveling to Palm Springs - Photo 8',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085928/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2656.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085928/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2656.jpg',
     title: 'Traveling to Palm Springs - Photo 9',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1770085927/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2657.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085927/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2657.jpg',
     title: 'Traveling to Palm Springs - Photo 10',
     width: 4,
     height: 3
   }
 ]
 
-export const phoenix = [
-  // Lindi's horse ranch in North Scottsdale
+export const palmSpringsFood = [
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1/chrisvogt-me/galleries/now-jan-2026/PLACEHOLDER-horseranch-1.jpg',
-    title: "Lindi's horse ranch - Photo 1",
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085925/chrisvogt-me/galleries/now-jan-2026/20260116-IMG_2647.jpg',
+    title: 'Palm Springs food - Photo 1',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085929/chrisvogt-me/galleries/now-jan-2026/20260116-IMG_2651.jpg',
+    title: 'Palm Springs food - Photo 2',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085927/chrisvogt-me/galleries/now-jan-2026/20260116-IMG_2652.jpg',
+    title: 'Palm Springs food - Photo 3',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085921/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2673.jpg',
+    title: 'Palm Springs food - Photo 4',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085918/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2702.jpg',
+    title: 'Palm Springs food - Photo 5',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085919/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2707.jpg',
+    title: 'Palm Springs food - Photo 6',
+    width: 3,
+    height: 4
+  }
+]
+
+export const drivingToPhoenix = [
+  // General Patton Memorial Museum and Quartzite
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085926/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2766.jpg',
+    title: 'General Patton Memorial Museum - Photo 1',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1/chrisvogt-me/galleries/now-jan-2026/PLACEHOLDER-horseranch-2.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085922/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2768.jpg',
+    title: 'General Patton Memorial Museum - Photo 2',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085928/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2771.jpg',
+    title: 'Quartzite - Photo 1',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2774.jpg',
+    title: 'Quartzite - Photo 2',
+    width: 3,
+    height: 4
+  }
+]
+
+export const godaddyTempe = [
+  // GoDaddy office on Mill Ave in Tempe
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085911/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2812.jpg',
+    title: 'GoDaddy Tempe office - Photo 1',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085915/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2814.jpg',
+    title: 'GoDaddy Tempe office - Photo 2',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085938/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2818.jpg',
+    title: 'GoDaddy Tempe office - Photo 3',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085942/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2823.jpg',
+    title: 'GoDaddy Tempe office - Photo 4',
+    width: 3,
+    height: 4
+  }
+]
+
+export const horseRanch = [
+  // Lindi's horse ranch in North Scottsdale
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085931/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2845.jpg',
+    title: "Lindi's horse ranch - Photo 1",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085921/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2871.jpg',
     title: "Lindi's horse ranch - Photo 2",
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1/chrisvogt-me/galleries/now-jan-2026/PLACEHOLDER-horseranch-3.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085920/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2877.jpg',
     title: "Lindi's horse ranch - Photo 3",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085932/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2879.jpg',
+    title: "Lindi's horse ranch - Photo 4",
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1/chrisvogt-me/galleries/now-jan-2026/PLACEHOLDER-horseranch-4.jpg',
-    title: "Lindi's horse ranch - Photo 4",
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085934/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2884.jpg',
+    title: "Lindi's horse ranch - Photo 5",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085918/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2888.jpg',
+    title: "Lindi's horse ranch - Photo 6",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085923/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2898.jpg',
+    title: "Lindi's horse ranch - Photo 7",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085916/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2900.jpg',
+    title: "Lindi's horse ranch - Photo 8",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085924/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2907.jpg',
+    title: "Lindi's horse ranch - Photo 9",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085924/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2913.jpg',
+    title: "Lindi's horse ranch - Photo 10",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085935/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2926.jpg',
+    title: "Lindi's horse ranch - Photo 11",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085933/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2931.jpg',
+    title: "Lindi's horse ranch - Photo 12",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085933/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2933.jpg',
+    title: "Lindi's horse ranch - Photo 13",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085942/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2944.jpg',
+    title: "Lindi's horse ranch - Photo 14",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085943/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2945.jpg',
+    title: "Lindi's horse ranch - Photo 15",
     width: 3,
     height: 4
   }
