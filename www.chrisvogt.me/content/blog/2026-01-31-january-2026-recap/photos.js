d371d1091754cfad55592d0eb21ee5caf1d1dc07
Chris Vogt <mail@chrisvogt.me>
2026-02-02 19:40:39 -0800

    SQUASHME: fixup add labels to all images

--- www.chrisvogt.me/content/blog/2026-01-31-january-2026-recap/photos.js
+++ www.chrisvogt.me/content/blog/2026-01-31-january-2026-recap/photos.js
@@ -4,31 +4,31 @@
 export const chinatown = [
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085939/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2393.jpg',
-    title: 'Chinatown - Photo 1',
+    title: "Ross Alley in San Francisco's Chinatown district",
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085940/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2397.jpg',
-    title: 'Chinatown - Photo 2',
+    title: 'Free samples at Golden Gate Fortune Cookie Factory',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085920/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2404.jpg',
-    title: 'Chinatown - Photo 3',
+    title: 'Nick posing in front of his year of the rooster sign',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085919/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2412.jpg',
-    title: 'Chinatown - Photo 4',
+    title: 'Nick is a dragon',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085913/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2416.jpg',
-    title: 'Chinatown - Photo 5',
+    title: 'Deep fried lobster balls at New Love Tea in Chinatown',
     width: 3,
     height: 4
   }
@@ -37,37 +37,37 @@ export const chinatown = [
 export const friendsInTheCity = [
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085939/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2426.jpg',
-    title: 'Out with friends - Photo 1',
+    title: 'Sean and Roc at Vesuvio Cafe',
     width: 4,
     height: 3
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2456.jpg',
-    title: 'Out with friends - Photo 2',
+    title: 'Songhan makes a grand entrance, visiting from NYC',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085917/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2458.jpg',
-    title: 'Out with friends - Photo 3',
+    title: 'Songhan says hello to Allen',
     width: 4,
     height: 3
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085914/chrisvogt-me/galleries/now-jan-2026/20260104-IMG_2472.jpg',
-    title: 'Out with friends - Photo 4',
+    title: 'Songhan, Chris (me) and Iván at The Mix',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085937/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2542.jpg',
-    title: 'Out with friends - Photo 5',
+    title: "Nick, Stephen and friends at Salim's belated NYE party",
     width: 4,
     height: 3
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085943/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2545.jpg',
-    title: 'Out with friends - Photo 6',
+    title: 'San Francisco backyard patio party in the Castro district',
     width: 3,
     height: 4
   }
@@ -76,43 +76,43 @@ export const friendsInTheCity = [
 export const outAndAbout = [
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085938/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2387.jpg',
-    title: 'Out and about - Photo 1',
+    title: 'The T light rail in the Central Subway',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085941/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2419.jpg',
-    title: 'Out and about - Photo 2',
+    title: 'The paperclip resistance movement at Vesuvio Cafe',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085936/chrisvogt-me/galleries/now-jan-2026/20260102-IMG_2435.jpg',
-    title: 'Out and about - Photo 3',
+    title: "The Tony Nik's banner in North Beach, SF",
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085916/chrisvogt-me/galleries/now-jan-2026/20260109-IMG_2499.jpg',
-    title: 'Out and about - Photo 4',
+    title: 'The J train at 30th St and Church in Noe Valley',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085925/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2513.jpg',
-    title: 'Out and about - Photo 5',
+    title: 'The 14 MUNI redirected through S Van Ness Ave in the Mission district',
     width: 4,
     height: 3
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2518.jpg',
-    title: 'Out and about - Photo 6',
+    title: 'Trinity Presbyterian Church, a Romanesque-style building in the Mission district',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085915/chrisvogt-me/galleries/now-jan-2026/20260110-IMG_2528.jpg',
-    title: 'Out and about - Photo 7',
+    title: '2026 Bingo Card—a very SF poster',
     width: 3,
     height: 4
   }
@@ -122,61 +122,61 @@ export const palmSprings = [
   // Traveling to Palm Springs
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085912/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2587.jpg',
-    title: 'Traveling to Palm Springs - Photo 1',
+    title: 'An Emirates A380 behind our tiny Embraer 175 (E175)',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085912/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2593.jpg',
-    title: 'Traveling to Palm Springs - Photo 2',
+    title: 'Nick being goofy in the seat in front of me',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085911/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2595.jpg',
-    title: 'Traveling to Palm Springs - Photo 3',
+    title: 'Alaska Airlines at SFO',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085936/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2599.jpg',
-    title: 'Traveling to Palm Springs - Photo 4',
+    title: 'Flying over South San Francisco',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085941/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2615.jpg',
-    title: 'Traveling to Palm Springs - Photo 5',
+    title: 'SFO from above',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085914/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2623.jpg',
-    title: 'Traveling to Palm Springs - Photo 6',
+    title: 'Welcome to Palm Springs sign at the airport',
     width: 4,
     height: 3
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085913/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2632.jpg',
-    title: 'Traveling to Palm Springs - Photo 7',
+    title: 'Palm Springs VillageFest, a Thursday night street festival downtown',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085922/chrisvogt-me/galleries/now-jan-2026/20260115-IMG_2641.jpg',
-    title: 'Traveling to Palm Springs - Photo 8',
+    title: 'Disco man scupture at Chill Bar Palm Springs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085928/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2656.jpg',
-    title: 'Traveling to Palm Springs - Photo 9',
+    title: 'Mount San Jacinto from Warm Sands, Palm Springs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085927/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2657.jpg',
-    title: 'Traveling to Palm Springs - Photo 10',
+    title: 'A 🏳️‍🌈 rainbow police cruiser in Palm Springs',
     width: 4,
     height: 3
   }
@@ -185,37 +185,37 @@ export const palmSprings = [
 export const palmSpringsFood = [
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085925/chrisvogt-me/galleries/now-jan-2026/20260116-IMG_2647.jpg',
-    title: 'Palm Springs food - Photo 1',
+    title: 'Guacamole at Casa Blanca Restaurant, Palm Springs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085929/chrisvogt-me/galleries/now-jan-2026/20260116-IMG_2651.jpg',
-    title: 'Palm Springs food - Photo 2',
+    title: 'Heart attack on a plate at Casa Blanca Restaurant, Palm Springs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085927/chrisvogt-me/galleries/now-jan-2026/20260116-IMG_2652.jpg',
-    title: 'Palm Springs food - Photo 3',
+    title: 'Casa Blanca Restaurant, Palm Springs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085921/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2673.jpg',
-    title: 'Palm Springs food - Photo 4',
+    title: 'A sparrow Nick found at Farm, Palm Springs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085918/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2702.jpg',
-    title: 'Palm Springs food - Photo 5',
+    title: 'Eggs Benedict and a duck omelette at Farm, Palm Springs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085919/chrisvogt-me/galleries/now-jan-2026/20260117-IMG_2707.jpg',
-    title: 'Palm Springs food - Photo 6',
+    title: 'Eggs Benedict at Farm, Palm Springs',
     width: 3,
     height: 4
   }
@@ -225,25 +225,25 @@ export const drivingToPhoenix = [
   // General Patton Memorial Museum and Quartzite
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085926/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2766.jpg',
-    title: 'General Patton Memorial Museum - Photo 1',
+    title: 'A tank at theGeneral Patton Memorial Museum',
     width: 4,
     height: 3
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085922/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2768.jpg',
-    title: 'General Patton Memorial Museum - Photo 2',
+    title: 'A tank, looking head on',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085928/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2771.jpg',
-    title: 'Quartzite - Photo 1',
+    title: 'A "jerky mine" in Quartzite, AZ',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085930/chrisvogt-me/galleries/now-jan-2026/20260119-IMG_2774.jpg',
-    title: 'Quartzite - Photo 2',
+    title: 'This peppered beed brisket jerky is the best I have ever had',
     width: 3,
     height: 4
   }
@@ -253,25 +253,25 @@ export const godaddyTempe = [
   // GoDaddy office on Mill Ave in Tempe
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085911/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2812.jpg',
-    title: 'GoDaddy Tempe office - Photo 1',
+    title: 'Catered lunch at the GoDaddy Tempe office',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085915/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2814.jpg',
-    title: 'GoDaddy Tempe office - Photo 2',
+    title: 'Looking north over the Salt River in Tempe',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085938/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2818.jpg',
-    title: 'GoDaddy Tempe office - Photo 3',
+    title: 'Cognite in Tempe, with an old-fashioned Spanish-style door',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085942/chrisvogt-me/galleries/now-jan-2026/20260122-IMG_2823.jpg',
-    title: 'GoDaddy Tempe office - Photo 4',
+    title: 'Looking south over Tempe from the GoDaddy office',
     width: 3,
     height: 4
   }
@@ -281,91 +281,91 @@ export const horseRanch = [
   // Lindi's horse ranch in North Scottsdale
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085931/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2845.jpg',
-    title: "Lindi's horse ranch - Photo 1",
+    title: 'Nick and the dogs at the horse ranch',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085921/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2871.jpg',
-    title: "Lindi's horse ranch - Photo 2",
+    title: 'She waits, she watches',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085920/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2877.jpg',
-    title: "Lindi's horse ranch - Photo 3",
+    title: 'Nick keeping watch over the puppy',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085932/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2879.jpg',
-    title: "Lindi's horse ranch - Photo 4",
+    title: "Lindi's ranch",
     width: 4,
     height: 3
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085934/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2884.jpg',
-    title: "Lindi's horse ranch - Photo 5",
+    title: 'Time with horses',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085918/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2888.jpg',
-    title: "Lindi's horse ranch - Photo 6",
+    title: 'Posing with horses',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085923/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2898.jpg',
-    title: "Lindi's horse ranch - Photo 7",
+    title: 'Nick guarding the puppy; or, the puppy guarding Nick',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085916/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2900.jpg',
-    title: "Lindi's horse ranch - Photo 8",
+    title: 'Lindi and the puppy',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085924/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2907.jpg',
-    title: "Lindi's horse ranch - Photo 9",
+    title: 'Aiden bringing in the horses',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085924/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2913.jpg',
-    title: "Lindi's horse ranch - Photo 10",
+    title: 'Up close with Fletcher, the horse',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085935/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2926.jpg',
-    title: "Lindi's horse ranch - Photo 11",
+    title: 'Matt greeting the dogs',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085933/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2931.jpg',
-    title: "Lindi's horse ranch - Photo 12",
+    title: 'Matt being pulled around by the puppy',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085933/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2933.jpg',
-    title: "Lindi's horse ranch - Photo 13",
+    title: 'Fridge art',
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085942/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2944.jpg',
-    title: "Lindi's horse ranch - Photo 14",
+    title: "Admiring Lindi's quilting project",
     width: 3,
     height: 4
   },
   {
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1770085943/chrisvogt-me/galleries/now-jan-2026/20260124-IMG_2945.jpg',
-    title: "Lindi's horse ranch - Photo 15",
+    title: 'The making of a quilt',
     width: 3,
     height: 4
   }
